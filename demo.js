37d31229708007db3e9250b39a0d69e62377242e
bhaveshparjapati164 <bhaveshparjapati164@gmail.com>
2026-06-18 17:37:35 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,18 @@
+const Name="Kartik"; //Variable cannot be re-declared or updated. A block scope variable.
+
+let age=18;    //Variable cannot be re-declared but can be updated
+
+var char='c';          // Variable can be re-declared & updated
+
+id=12;        //Global Variable
+
+//Name="Sahil";  Not allowed we can not change const variable value
+console.log(Name);
+
+age = 20;
+
+char='B';
+
+id=101;
+
+console.table([Name,age,char,id]);
